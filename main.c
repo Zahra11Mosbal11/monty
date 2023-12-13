17da6d14808c15bdf0d83ee7cbbf42cd7b1964fc
Zahra11Mosbal <zahramosbal@gmail..com>
2023-12-13 01:04:51 -0800

    monty

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+/**
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char *file_cont;
+	size_t n = 0;
+	ssize_t line = 1;
+	stack_t *stack = NULL;
+	unsigned int count = 0;
+
+	if (argc != 2)
+	/*If the user does not give any file or more than one argument */
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");/* open file on read*/
+	if (!file)/*if not possible to open the file*/
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (line > 0)/*get the file contener*/
+	{
+		file_cont = NULL;
+		line = getline(&file_cont, &n, file);
+		count++;
+		if (line > 0)
+		{
+			printf("%s\n%d", file_cont, count);/* this line will rmove it*/
+		/* we call the execute function*/
+		}
+		free(file_cont);
+	}
+	free(stack);
+	fclose(file);
+	return (0);
+}
